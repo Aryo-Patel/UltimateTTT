8add64fb5cb7cc2d721e0e9909ea8e56925fd503
Aryo-Patel <aryopatel@gmail.com>
2020-02-12 23:06:22 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+//TODO: Start working on creating a 9x9 TTT game...try creating a new js
+//file for it. 
 let board = document.getElementById("board-area");
 
 class ticTacToe{
     turn = ['red', '#007eff'];
     index = 0;
+    text_board = new Array(3);
+
     constructor(){
+        this.growboard();
         this.makeSquares();
     }
-
+    growboard(){
+        for(let i = 0; i < this.text_board.length; i ++){
+            this.text_board[i] = new Array(3);
+        }
+    }
 
     makeSquares(){
 
@@ -77,10 +86,61 @@ class ticTacToe{
             let squareClone = square.cloneNode(true);
             square.parentNode.replaceChild(squareClone, square);
             squareClone.style.backgroundColor = this.turn[this.index];
-            console.log(squareClone);
+            this.squareClass(square, this.turn[this.index]);
+            this.checkWin(this.turn[this.index]);
             this.index = (this.index + 1)%2;
+
         });
         return square;
+    }
+    squareClass(square, color) {
+        let boardNumber = square.classList[1];
+        this.text_board[parseInt(boardNumber/3)][boardNumber%3] = color;
+    }
+    checkWin(color){
+        let goalColor = color;
+        for(let i = 0; i < this.text_board.length; i ++){
+            let count = 0;
+            for(let j = 0; j < this.text_board[i].length; j++){
+                if(this.text_board[i][j] !== goalColor){
+                    console.log(`No one won in row ${i}`);
+                    count = 1;
+                }
+            }
+            if(count == 0){
+                console.log(`Winner in row ${i}`);
+            }
+        }
+        for(let i = 0; i < this.text_board.length; i++){
+            let count = 0;
+            for(let j = 0; j < 3; j++){
+                if(this.text_board[j][i] !== goalColor){
+                    console.log(`No one won in column ${i}`);
+                    count = 1;
+                }
+            }
+            if(count == 0){
+                console.log(`Winner in column ${i}`);
+            }
+        }
+        let countMain = 0;
+        let countOpp = 0;
+        for(let i = 0; i < 3; i++){
+
+            if(this.text_board[i][i] !== goalColor){
+                console.log(`No one won on main diagonal`);
+                countMain = 1;
+            }
+
+            if(this.text_board[i][2-i] !== goalColor){
+                console.log(`No one won on opposite diagonal`);
+                countOpp = 1;
+            }
+
+        }
+        if(countMain === 0|| countOpp ===0){
+            console.log(`Winner on a diagonal`);
+        }
     }
 }
 
